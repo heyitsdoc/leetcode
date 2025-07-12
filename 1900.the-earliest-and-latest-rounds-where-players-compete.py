310d2ef947d18daa7a231abcd585a889062f8793
heyitsdoc <deepaksaini45643@gmail.com>
2025-07-13 01:12:02 +0530

    daily challange 1

---
+++ 1900.the-earliest-and-latest-rounds-where-players-compete.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1900 lang=python3
+#
+# [1900] The Earliest and Latest Rounds Where Players Compete
+#
+
+# @lc code=start
+class Solution:
+    def earliestAndLatest(self, n: int, firstPlayer: int, secondPlayer: int) -> List[int]:
+        
+# @lc code=end
+
