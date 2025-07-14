d55c10de5bfffaecabcdf6d3885193685f3d64de
heyitsdoc <deepaksaini45643@gmail.com>
2025-07-14 15:51:09 +0530

    adding a test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+head = 0b101
+print(head)  # Output: 5, since 0b101 (binary) is 5 in decimal # Print head as decimal
+
+# result = head << 
+# print(result) 
+# print(bin(result)) # Output: 202, which is 101 in binary shifted left by 1 position
