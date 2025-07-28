e01b501fccdb7be1456cafac2246508a947f5213
heyitsdoc <deepaksaini45643@gmail.com>
2025-07-28 23:37:45 +0530

    day 17

---
+++ 2044.count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=2044 lang=python3
+#
+# [2044] Count Number of Maximum Bitwise-OR Subsets
+#
+
+# @lc code=start
+class Solution:
+    def countMaxOrSubsets(self, nums):
+        maxOR = 0
+        for num in nums:
+            maxOR |= num
+
+        def backtrack(index, currentOR):
+            if index == len(nums):
+                return 1 if currentOR == maxOR else 0
+
+            if currentOR == maxOR:
+                return 1 << (len(nums) - index)
+
+            return backtrack(index + 1, currentOR | nums[index]) + \
+                   backtrack(index + 1, currentOR)
+
+        return backtrack(0, 0)  
+# @lc code=end
+
