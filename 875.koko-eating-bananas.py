a29a7ca4bfc45d05245426810b9f0b6a72d89f56
heyitsdoc <deepaksaini45643@gmail.com>
2025-08-04 22:38:03 +0530

    day 24

---
+++ 875.koko-eating-bananas.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=875 lang=python3
+#
+# [875] Koko Eating Bananas
+#
+
+# @lc code=start
+from collections import defaultdict
+
+class Solution:
+    def totalFruit(self, fruits: list[int]) -> int:
+        start = 0
+        max_len = 0
+        fruit_count = defaultdict(int)
+
+        for end in range(len(fruits)):
+            fruit_count[fruits[end]] += 1
+
+            while len(fruit_count) > 2:
+                fruit_count[fruits[start]] -= 1
+                if fruit_count[fruits[start]] == 0:
+                    del fruit_count[fruits[start]]
+                start += 1
+
+            max_len = max(max_len, end - start + 1)
+
+        return max_len     
+# @lc code=end
+
