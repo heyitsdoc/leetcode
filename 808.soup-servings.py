c05d9cd57d736e169f515ec0c7e2189e66675178
heyitsdoc <deepaksaini45643@gmail.com>
2025-08-08 23:47:43 +0530

    day 28

---
+++ 808.soup-servings.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=808 lang=python3
+#
+# [808] Soup Servings
+#
+
+# @lc code=start
+class Solution:
+    def f(self, a, b, dp):
+        if a <= 0 and b > 0:
+            return 1.0
+        if a == 0 and b == 0:
+            return 0.5
+        if a > 0 and b <= 0:
+            return 0.0
+        if a <= 0 and b <= 0:
+            return 0.5
+        if dp[a][b] != -1:
+            return dp[a][b]
+
+        x = 0.25 * self.f(a - 100, b, dp)
+        y = 0.25 * self.f(a - 75, b - 25, dp)
+        z = 0.25 * self.f(a - 50, b - 50, dp)
+        w = 0.25 * self.f(a - 25, b - 75, dp)
+
+        dp[a][b] = x + y + z + w
+        return dp[a][b]
+
+    def soupServings(self, n):
+        if n > 4800:
+            return 1.0
+        dp = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
+        return self.f(n, n, dp)
+     
+# @lc code=end
+
